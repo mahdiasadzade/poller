14dc147a61895318f1a0c1d2f4a8014878810cd1
mahdiasadzade <132713308+mahdiasadzade@users.noreply.github.com>
2025-08-31 14:04:42 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import os
 from telegram.ext import Updater, MessageHandler, Filters
 
-# گرفتن توکن از Environment Variable
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-# جایی که میخوای پیام‌ها بره (مثلا آیدی خودت یا گروه)
-FORWARD_CHAT_ID = os.getenv("FORWARD_CHAT_ID")
+
+# لیست مبداها (source) با کاما جدا می‌کنیم
+SOURCE_CHAT_IDS = os.getenv("SOURCE_CHAT_IDS", "*").split(",")
+
+# لیست مقصدها
+DEST_CHAT_IDS = os.getenv("DEST_CHAT_IDS", "").split(",")
 
 def forward_message(update, context):
-    context.bot.forward_message(
-        chat_id=FORWARD_CHAT_ID,
-        from_chat_id=update.message.chat_id,
-        message_id=update.message.message_id
-    )
+    from_id = update.message.chat_id
+    # اگر * بود، همه رو قبول کن
+    if SOURCE_CHAT_IDS != ["*"] and str(from_id) not in SOURCE_CHAT_IDS:
+        return
+
+    for chat_id in DEST_CHAT_IDS:
+        chat_id = int(chat_id.strip())
+        context.bot.forward_message(
+            chat_id=chat_id,
+            from_chat_id=from_id,
+            message_id=update.message.message_id
+        )
 
 if __name__ == "__main__":
     updater = Updater(token=BOT_TOKEN, use_context=True)
